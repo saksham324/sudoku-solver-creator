0cc3d6e0551f077baefc060e3da4824414ecf81e
Sayuri Tais Miyamoto Magnabosco <f0036xk@plank.thayer.dartmouth.edu>
2021-11-05 12:55:20 -0400

    main code file

---
+++ sudoku.c
@@ -0,0 +1,22 @@
+/* sudoku.c - 
+* 
+* Authors:
+* November 5th, 2021
+* CS50 Fall 2021, Final Project
+*
+*/ 
+
+#include <stdio.h>
+
+/***************** Local global variables ***************/
+
+/******************** Data structures *******************/
+
+/**************** Local function prototypes **************/
+
+/***********************  Main  **************************/
+
+int main(const int argc, char *argv[]){
+
+    exit 0;
+}
