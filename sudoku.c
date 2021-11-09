b6b62e04e8a3d006ef837c110edd93dcbd9241f6
Sayuri Tais Miyamoto Magnabosco <f0036xk@plank.thayer.dartmouth.edu>
2021-11-09 00:04:26 -0500

    validation done

--- sudoku.c
+++ sudoku.c
@@ -1,23 +1,51 @@
 /* sudoku.c - 
 * 
-* Authors:
+* Authors: The C Crew - Brody T., Saksham A., Sayuri M. 
+* CS50 Fall 2021, Final Project 
 * November 5th, 2021
-* CS50 Fall 2021, Final Project
 *
 */ 
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <common/common.h>
+#include <common/validateInput.h>
 
 /***************** Local global variables ***************/
-
-/******************** Data structures *******************/
+static const int BOARD_SIZE = 9;  // how long each row and column will be (e.g. 9 for 9x9 grid)
 
 /**************** Local function prototypes **************/
 
 /***********************  Main  **************************/
 
 int main(const int argc, char *argv[]){
+    char *mode;          // string input set to `create` or `solve`
+    char *difficulty;    // string input set to `easy` or `hard`
+
+    // Validate arguments:
+    // If number of input arguments is wrong
+    if (argc != 3){  
+        fprintf(stderr, "Usage: ./sudoku mode difficulty\n\n");
+		exit(1);
+    }
+    // Checks if the input mode is a valid one 
+    if (!isValidMode(argv[1])) {
+        fprintf(stderr, "Error: invalid mode option. You can pick between 'create' or 'solve'\n\n");
+        exit(2);
+    }
+    // Checks if the input difficulty is a valid one (easy/hard)
+    if (!isValidDifficulty(argv[2])) {
+        fprintf(stderr, "Error: invalid difficulty option. You can pick between 'easy' or 'hard'\n\n");
+        exit(3);
+    }
+
+    // Since the inputs are correct, we can assign them into variables:
+    mode = argv[1];
+    difficulty = argv[2];
+
+    sudoku_board_t board = generateEmptyBoard(BOARD_SIZE);
 
     exit 0;
 }
