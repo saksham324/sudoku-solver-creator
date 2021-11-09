ce89473c4b3db6288438fe989415791a9ccbedd9
Saksham Arora <f0042mm@plank.thayer.dartmouth.edu>
2021-11-08 21:17:13 -0500

    added header file and implemented loadBoard

---
+++ solver/solver.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include "../common/common.h"
+#include "../creator/creator.h"
+
+/************ loadBoard ************/
+
+/*
+ * Loads board from stdin and validates loaded board
+ * 
+ * Caller provides:
+ *  Valid file pointer to read from
+ * Function guarantee:
+ *  Returned board is valid or return NULL
+ * Caller is responsible for:
+ *  Calling deleteBoard on the returned sudoku_board
+ */
+sudoku_board *loadBoard(FILE *fp) {
+  if (!fp) return NULL;
+
+  sudoku_board *lBoard = generateEmptyBoard(); // generate Empty Board to be filled out 
+  if (!lBoard) return NULL;
+  
+  // make sure number of rows is valid, iterate through each row
+  int rowCount = 0;
+  while (rowCount < lBoard->size) {
+
+    // Iterate through each column
+    int colCount = 0;
+    while (colCount < lBoard->size) {
+
+      // Get each character from fp, check if char is a digit, if yes insert
+      char ch = fgetc(fp);
+      if (ch == '\n') { 
+          rowCount++; 
+          break; 
+        }
+      if (!isdigit(ch)) { 
+          continue; 
+          }
+
+      // Convert char to int value (ASCII)
+      lBoard->boardArray[rowCount][colCount] = (int)ch - 48;
+      colCount++;
+    }
+  }
+
+  return lBoard;
+}
---
+++ solver/solver.h
@@ -0,0 +1,40 @@
+/* 
+* Header file for Solver module
+*/ 
+
+#include <stdio.h> 
+#include "../common/common.h"
+
+/************ solveBoard ************/
+/*
+ * Solves a passed board and returns whether or not the solution is unique
+ * Caller provides:
+ *  A valid sudoku_board
+ * We guarantee:
+ *  The passed board is solved with one solution or false is returned
+ * Caller is responsible for:
+ *  Nothing
+ */
+
+int *solveBoard(sudoku_board *board); 
+
+/************ loadBoard ************/
+
+/*
+ * Loads board from stdin and validates loaded board
+ * 
+ * Caller provides:
+ *  Valid file pointer to read from
+ * Function guarantee:
+ *  Returned board is valid or return NULL
+ * Caller is responsible for:
+ *  Calling deleteBoard on the returned sudoku_board
+ */
+sudoku_board *loadBoard(FILE *fp);
+
+
+
+
+
+
+
